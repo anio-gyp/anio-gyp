13dfcfa6d6805cca27daae2ce8f63d3bfe64c46a
dasdeo <dasdeo@proton.me>
2023-12-26 19:07:27 +0100

    Automatically clean src/auto folder for projects.

--- src/main.mjs
+++ src/main.mjs
@@ -1,6 +1,12 @@
 import lib from "./type/lib/index.mjs"
 
+import {
+	cleanAutoDirectory
+} from "@anio-jsbundler/core"
+
 export default async function(project) {
+	await cleanAutoDirectory(project)
+
 	if (project.config.type === "lib") {
 		await lib(project)
 	}
