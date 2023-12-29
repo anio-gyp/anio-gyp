c4f3c9e5c1b293b93b9d47d37778098c4837311f
dasdeo <dasdeo@proton.me>
2023-12-29 23:19:43 +0100

    Print a comment about the end result.

--- src/main.mjs
+++ src/main.mjs
@@ -8,6 +8,8 @@ import scrubFiles from "./lib/step/scrubFiles.mjs"
 import generateAutoFiles from "./lib/step/generateAutoFiles.mjs"
 import invokeBundler from "./lib/step/invokeBundler.mjs"
 
+import {colorize} from "@anio-jsbundler/utilities"
+
 export default async function(options, project) {
 	await printProjectSummary(options, project)
 	await printWarnings(options, project)
@@ -18,4 +20,14 @@ export default async function(options, project) {
 	await scrubFiles(options, project)
 	await generateAutoFiles(options, project)
 	await invokeBundler(options, project)
+
+	if (project.warnings.length) {
+		process.stderr.write(
+			colorize("yellow.bold", `⚠️  Bundling complete, but make sure to read the warnings above.\n`)
+		)
+	} else {
+		process.stderr.write(
+			colorize("green.bold", `✅ Bundling complete, no warnings.\n`)
+		)
+	}
 }
