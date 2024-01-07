b46e27dc1709184503f7ce436a12a6fbb0b0dca1
dasdeo <dasdeo@proton.me>
2024-01-07 20:45:01 +0100

    Remove update list.

--- src/phase/0.update/list/github_cicd_workflow/index.mjs
+++
@@ -1,9 +0,0 @@
-import checkGitHubCICDWorkflow from "./checkGitHubCICDWorkflow.mjs"
-
-export default {
-	label: "GitHub CI/CD Workflow",
-
-	async run(project) {
-		await checkGitHubCICDWorkflow(project)
-	}
-}
--- src/phase/0.update/list/index.mjs
+++
@@ -1,7 +0,0 @@
-import github_cicd_workflow from "./github_cicd_workflow/index.mjs"
-import node_module from "./node_module/index.mjs"
-
-export default [
-	github_cicd_workflow,
-	node_module
-]
