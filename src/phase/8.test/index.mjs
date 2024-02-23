477f557a8e8b352e2169351459f87b7b9921729d
dasdeo <mark@anio.software>
2024-02-23 09:36:57 +0100

    Add experimental support for unit testing with anio-jtest.

--- src/phase/8.test/index.mjs
+++ src/phase/8.test/index.mjs
@@ -1,6 +1,10 @@
 import print from "../../lib/print.mjs"
 
 import {colorize} from "@anio-gyp/utilities"
+import {
+	createJTestSession,
+	expandAndValidateInputTestFiles
+} from "anio-jtest"
 
 export default {
 	id: "test",
@@ -18,6 +22,54 @@ export default {
 			)
 
 			return
+		}
+
+		const test_files = await expandAndValidateInputTestFiles(
+			project.root, project.config.test.input
+		)
+
+		const jtest_session = createJTestSession({
+			project_root: project.root,
+			test_files
+		})
+
+		jtest_session.on("report", ({id, value}) => {
+			if (id !== "test_result") return
+
+			const {test, results} = value
+
+			let all_done = results.filter(r => r === false).length === 0
+
+			if (!all_done) return
+
+			print(`    `)
+
+			const pass = colorize("green", "✔")
+			const fail = colorize("red", "✘")
+			const error = colorize("yellow", "⚠")
+			const timeout = colorize("red", "⏱")
+
+			for (const result of results) {
+				if (result.has_error_occurred_during_testing) {
+					print(`${error} `)
+				} if (result.verdict === "pass") {
+					print(`${pass} `)
+				} else {
+					print(`${fail} `)
+				}
+			}
+
+			print(`${colorize("gray", test.label)}\n`)
+		})
+
+		const result = await jtest_session.run()
+
+		if (!result.successful) {
+			print(
+				colorize("red.bold", `\n⛔ Stopping here, some unit tests have not passed.\n`)
+			)
+
+			process.exit(2)
 		}
 	}
 }
